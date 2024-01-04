cfeabef8f646aa1b1a914678d5f7e41fbc9ecee4
Aarya <falconblaze90@gmail.com>
2024-01-04 13:41:49 +0530

    Added templates about & home

---
+++ blog/templates/Readme.md
@@ -0,0 +1,3 @@
+By Default Django searches for templates so when we crate more apps 
+it might get confusing which templates folder is which so we create 
+a sub folder with same name as app.
--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,30 @@ from django.http import HttpResponse
 
 # Create your views here.
 # How we want to handle server routes
+posts =[
+    {
+        'author': 'Aarya',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '4 January, 2024'
+    },
+    {
+        'author': 'User12',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '2 January, 2024'
+    }
+
+]
 
 
 def home(request):
-    return HttpResponse("</h1>Blog-Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog-About</h1>")
+    return render(request, 'blog/about.html', {'title': 'About'})
 
