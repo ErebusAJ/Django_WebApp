86300c3ff05e25411c23e210ec9d9311d318fdff
Aarya <falconblaze90@gmail.com>
2024-01-04 12:29:43 +0530

    Home & About Url Routing

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# How we want to handle server routes
+
+
+def home(request):
+    return HttpResponse("</h1>Blog-Home</h1>")
+
+
+def about(request):
+    return HttpResponse("<h1>Blog-About</h1>")
+
