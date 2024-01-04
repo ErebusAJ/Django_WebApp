808065fe9e083f75eb7ab5a13f5e430c8af470bb
Aarya <falconblaze90@gmail.com>
2024-01-04 18:59:20 +0530

    Created superuser

--- blog/templates/Readme.md
+++ blog/templates/Readme.md
@@ -1,3 +1,3 @@
-By Default Django searches for templates so when we crate more apps 
-it might get confusing which templates folder is which so we create 
+By Default Django searches for templates so when we create more apps 
+it might get confusing which templates folder is which, so we create 
 a sub folder with same name as app.
--- blog/views.py
+++ blog/views.py
@@ -7,14 +7,34 @@ posts =[
     {
         'author': 'Aarya',
         'title': 'Blog Post 1',
-        'content': 'First post content',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut '
+                   'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris '
+                   'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate '
+                   'velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non '
+                   'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         'date_posted': '4 January, 2024'
     },
     {
         'author': 'User12',
         'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': '2 January, 2024'
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut '
+                   'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris '
+                   'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate '
+                   'velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non '
+                   'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'date_posted': '2 January, 2024',
+
+    },
+    {
+        'author': 'Jamwal',
+        'title': 'Blog post 3',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut '
+                   'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris '
+                   'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate '
+                   'velit esse cillum dolore eu fugiat nulla paginator. Excepteur sint occaecat cupidatat non '
+                   'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'date_posted': '1 January, 2024',
+
     }
 
 ]
