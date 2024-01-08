0e6e95601676b49102dddf115d44b2e496d249ba
Aarya <falconblaze90@gmail.com>
2024-01-09 00:06:26 +0530

    Environ Var

--- project/settings.py
+++ project/settings.py
@@ -141,6 +141,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'brooksoulking008@gmail.com'
-EMAIL_HOST_PASSWORD = 'jjcl dmee zjkg rrtk'
+EMAIL_HOST_USER = os.environ.get('USER_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('USER_PASS')
 
